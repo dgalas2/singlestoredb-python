67283e6c4e64051b3ea3691094a4bcc9f8452027
Revanth Nalla <rnalla@singlestore.com>
2025-02-28 11:51:39 +0530

    run_pex.py

--- singlestoredb/apps/run_pex.py
+++ singlestoredb/apps/run_pex.py
@@ -4,17 +4,21 @@ import requests
 import subprocess
 from ._config import AppConfig
 
+status = False
 def ping_port(port, timeout):
     url = f'http://0.0.0.0:{port}/'
     start_time = time.time()
-    
+
+    global status
     while time.time() - start_time < timeout:
         response = requests.get(url, timeout = 5)  # 5-second timeout for each request
         if response.status_code == 200:
+            status = True
             return True
         
         time.sleep(1)  # Wait before trying again
 
+    status = False
     return False
 
 def run_pex_app(
@@ -25,10 +29,12 @@ def run_pex_app(
     app_config = AppConfig.from_env()
     port = app_config.listen_port
 
-    process = subprocess.Popen(['python', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(port)
+    process = subprocess.Popen(['python3', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(process.pid)
     ping_thread = threading.Thread(target=ping_port, args=(port,time_out))
     ping_thread.start()
 
     ping_thread.join()
 
-    return ping_thread.is_alive()
+    return status

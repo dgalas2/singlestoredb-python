e17c68047a60c396d62133a67b38315d78a71cbd
Revanth Nalla <rnalla@singlestore.com>
2025-02-28 11:20:01 +0530

    remove host arg

--- singlestoredb/apps/run_pex.py
+++ singlestoredb/apps/run_pex.py
@@ -4,8 +4,8 @@ import requests
 import subprocess
 from ._config import AppConfig
 
-def ping_port(host, port, timeout):
-    url = f'http://{host}:{port}/'
+def ping_port(port, timeout):
+    url = f'http://0.0.0.0:{port}/'
     start_time = time.time()
     
     while time.time() - start_time < timeout:
@@ -18,18 +18,17 @@ def ping_port(host, port, timeout):
     return False
 
 def run_pex_app(
-    host,
 	file_path : str,
 	time_out : int,
 ) -> bool:
 	
-	app_config = AppConfig.from_env()
-	port = app_config.listen_port
- 
-	process = subprocess.Popen(['python', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    	ping_thread = threading.Thread(target=ping_port, args=(host, port,time_out))
-   	ping_thread.start()
-    
-    	ping_thread.join()
+    app_config = AppConfig.from_env()
+    port = app_config.listen_port
 
-    	return ping_thread.is_alive()
+    process = subprocess.Popen(['python', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ping_thread = threading.Thread(target=ping_port, args=(port,time_out))
+    ping_thread.start()
+
+    ping_thread.join()
+
+    return ping_thread.is_alive()
